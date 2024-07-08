54248a33dd9f75c1901816a5ca51d36e44f6e67b
coreyabrown <coreyabrown.cb@gmail.com>
2024-07-08 00:36:16 -0500

    Update Focus Theme For Menu Buttons

--- Scenes/StartMenuUI.gd
+++ Scenes/StartMenuUI.gd
@@ -7,8 +7,10 @@ func _ready():
 
 
 func _on_exit_pressed():
+	# Haptic feedback on button press?
 	get_tree().quit()
 
 
 func _on_new_game_pressed():
+	# Haptic feedback on button press?
 	get_tree().change_scene_to_file("res://Scenes/LivingRoom.tscn")
