beb45c6cb60de721214a0d6620e515555895d936
coreyabrown <coreyabrown.cb@gmail.com>
2024-07-10 23:18:19 -0500

    Update to New HUD

--- Scenes/SceneHelpers/3DHUD.gd
+++ Scenes/SceneHelpers/3DHUD.gd
@@ -1,30 +1,31 @@
-extends CanvasLayer
+extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_score(0)
 
+
 func update_score(score):
 	var score_bug = "Score: "+str(score)
-	$Container/VBoxContainer/ScoreLabel.text = str(score_bug)
+	$ScoreLabel.text = str(score_bug)
 
 
 func _on_score_area_update_combo_text(new_value):
 	if new_value.size() == 1:
-		$Container/VBoxContainer/ComboLabel.text = str(new_value[0])
+		$ComboLabel.text = str(new_value[0])
 	elif new_value.size() > 1:
-		var combo_text = $Container/VBoxContainer/ComboLabel.text
+		var combo_text = $ComboLabel.text
 		for i in range(new_value.size()):
 			if i > 0:
 				combo_text += " * " + str(new_value[i])
-		$Container/VBoxContainer/ComboLabel.text = str(combo_text)
+		$ComboLabel.text = str(combo_text)
 
 
 func _on_score_area_update_combo_score(new_value):
-	$Container/VBoxContainer/ComboScoreLabel.text = str(new_value)
+	$ComboScore.text = str(new_value)
 
 
 func _on_score_area_combo_end(score):
-	$Container/VBoxContainer/ComboLabel.text = ""
-	$Container/VBoxContainer/ComboScoreLabel.text = ""
+	$ComboLabel.text = ""
+	$ComboScore.text = ""
 	update_score(score)
